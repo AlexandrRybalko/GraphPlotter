cf8ddc0d534065a94ff1228b91ffeaf20ab10946
AlexandrRybalko <rybalkosanya20@gmail.com>
2025-03-25 14:47:11 +0000

    added input validation

--- GraphPlotter/MainWindow.xaml.cs
+++ GraphPlotter/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using GraphPlotter.ViewModels;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GraphPlotter
 {
     public partial class MainWindow : Window
     {
+        private Regex numericRregex = new Regex("^-?[1-9]+\\.?[0-9]*$");
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +21,19 @@ namespace GraphPlotter
         {
             var vm = DataContext as MainViewModel;
             vm.SaveState();
+        }
+
+        private void TextBlockPreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                var newText = textBox.Text.Insert(textBox.CaretIndex, e.Text);
+
+                if (!numericRregex.IsMatch(newText))
+                {
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
